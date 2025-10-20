c39d103596afdf9b23f20ec6c15e768f1da5f564
Luis Serna <louieserna44@yahoo.com>
2025-10-20 10:08:27 -0500

    added score to homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue'
 import BaseTitle from '../components/BaseTitle.vue';
 import useAPI from '../composables/useAPI'
+import mainScore from '@/components/mainScore.vue';
 
 const {categories, getCategories} = useAPI()
 
@@ -13,7 +14,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+  <BaseTitle>TRIVIA APPLICATION - <mainScore></mainScore></BaseTitle>
 
   <div class="grid flex-grow grid-cols-4 gap-12 m-20">
     <RouterLink
